4bccb8a9c69601506928b8f792dd2b981560334b
Sambhavgoel <sambhavgoelyash0907@gmail.com>
2025-03-07 12:16:49 +0530

    crud

--- Day4/app2.js
+++ Day4/app2.js
@@ -1,33 +1,70 @@
-const express = require('express')
-const fs = require('promises/fs')
+const express=require('express');
+const fs=require('fs/promises');
 
-const app = express()
+const app=express();
 
-app.use(express.json())
+let users=[];
+app.use(express.json());
 
-let users=[]
-
-const readdata = ()=>{
-    users = fs.readFile('./data.json','utf-8')
-
+const readdata=async ()=>{
+    users=JSON.parse(await fs.readFile('./data.json','utf-8'))
 }
-
-const writedata = ()=>{
-    fs.writedata('./data.json',JSON.stringify(users,null,2))
-
+const writedata=async ()=>{
+    await fs.writeFile('./data.json',JSON.stringify(users))
 }
+readdata();
 
-app.get('./getdata',async(req,res)=>{
-    readdata()
-    res.json(users)
+app.get('/getdata',async (req,res)=>{
+    res.json(users);
 })
 
-app.listen(9000,(err)=>{
-    if(err)
+app.post('/users',(req,res)=>{
+    const {name,age}=req.body;
+    const newid=users.length>0?users[users.length-1].id+1:1;
+    const newuser={id:newid,name,age};
+    users.push(newuser);
+    writedata();
+    res.status(200).json({message: 'User Register Success',data:newuser});
+})
+
+app.put('/users/:id',(req,res)=>{
+    const uid=parseInt(req.params.id);
+    const {name,age}=req.body;
+    const userIndex=users.findIndex((user)=>user.id===uid);
+    if (!name || !age)
     {
-        console.log(err)
-        return
+        res.status(400).json({message: 'name and age are required'})
     }
-    console.log('Server is running on port 9000')
+    if (userIndex===-1)
+    {
+        res.status(404).json({message: 'user not found'});
+    }
+    else{
+        users[userIndex].name=name;
+        users[userIndex].age=age;
+        writedata();
+        res.status(200).json({message:"user updated successfully", data:users[userIndex]});
+    }
 })
 
+app.delete('/users/:id',(req,res)=>{
+    const uid=req.params.id;
+    console.log(uid)
+    const userIndex=users.findIndex((user)=>user.id==uid);
+    if (userIndex==-1)
+    {
+        return res.status(404).json({message: 'user not found'});
+    }
+    else{
+        users.splice(userIndex,1);
+        writedata();
+        res.status(200).json({message:"user deleted successfully"});
+    }
+
+})
+
+app.listen(9000,(e)=>{
+    if (e)
+        console.log(e)
+    console.log("Port is running on 9000");
+})
