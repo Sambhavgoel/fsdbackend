5e3c3256292abe93b937a3368bc886fb30be79fc
Sambhavgoel <sambhavgoelyash0907@gmail.com>
2025-03-21 12:13:28 +0530

    frontend-backend

---
+++ Day5/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from './components/Register'
+
+function App() {
+  return (
+    <div>
+      <h1>User Registration System</h1>
+      <Register/>
+    </div>
+  )
+}
+
+export default App
---
+++ Day5/Frontend/src/components/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios'
+
+const Register = ()=>{
+    const handleregister = async(e)=>{
+        e.preventDefault()
+        const user = {
+            name : e.target.name.value,
+            age : e.target.age.value
+        }
+        await axios.post('http://localhost:9000/users',user)
+        alert('User Registered Successfully')
+    }
+
+  return (
+    <div>
+        <form action="" onSubmit={handleregister}>
+
+            <label>Name :</label> <input type="text" name='name'/>
+            <label>Age :</label> <input type="number" name='age'/>
+            <button type='submit'>Register</button>
+        
+        </form>
+    </div>
+  )
+
+}
+
+
+export default Register
